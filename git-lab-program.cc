f917236e8bc4e9f716f570c388b3caf316d24445
Joshua Onk <jo988618@ohio.edu>
2020-02-12 16:33:17 -0500

    Initial Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Joshua Onk
+ *        Date: Febuary 5 2019
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
